f2be68daeda60c7592db43126b26fdb55f3eb728
beki <bilguunganzurh@gmail.com>
2025-08-21 22:27:14 +0800

    feat: test fetch

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,15 @@
-export default function Home() {
+const Home = async () => {
+  const res = await fetch("https://api.gpt.bilguun.tech/endpoint");
+  const data = await res.json();
+
   return (
     <div>
       <main>
         <h1>Super Cool GPT</h1>
+        <p>{data.message}</p>
       </main>
     </div>
   );
-}
+};
+
+export default Home;
