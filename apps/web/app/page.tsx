8a15850e109e8a3bb76345121c04eec7ac5c2048
beki <bilguunganzurh@gmail.com>
2025-08-21 23:55:40 +0800

    feat: api test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,12 @@
 const Home = async () => {
-  const res = await fetch("https://api.gpt.bilguun.tech/endpoint");
+  const res = await fetch("https://api.gpt.bilguun.tech");
   const data = await res.json();
 
   return (
     <div>
       <main>
         <h1>Super Cool GPT</h1>
-        <p>{data.message}</p>
+        <p>{JSON.stringify(data)}</p>
       </main>
     </div>
   );
